83621ff88cbdb6456d7ade6f0566e62f5b873102
Piet Geursen <pietgeursen@gmail.com>
2015-01-24 20:34:55 +1300

    Refactor some variable names.

--- searchConversation.rb
+++ searchConversation.rb
@@ -1,8 +1,11 @@
-
-
 def getNumOfOccurancesOfString(line, string)
 	regex = Regexp::new(string)
-	line.scan(regex).length 	
+	
+	line.scan(regex).length 
+	# puts line
+	# puts string
+	# line.to_s.count string	
+
 end
 
 def checkSender(line, sender)
@@ -22,9 +25,9 @@ def scanForStartOfEmail(f)
 	while (line = f.gets)
 		if isEmailStart(line)
 			if checkSender(line,  'Piet')
-				puts "Piet"
+				@sender = "Piet"
 			else
-				puts "Babs"
+				@sender = "Babs"
 			end
 
 			break
@@ -34,34 +37,44 @@ def scanForStartOfEmail(f)
 	f
 end
 
-def processFile(fileToOpen)
+def searchEmailForNumOccurances(fileToOpen, searchTerm)
 
-	count = 0
+	numOccurances = 0
 
-	File.open(fileToOpen, 'r') do |f1| 
+	File.open(fileToOpen, 'r') do |file| 
 
-		f1 = scanForStartOfEmail f1
+		file = scanForStartOfEmail file
 
-		while line = f1.gets
-			count += getNumOfOccurancesOfString line, '!'
+		while line = file.gets
+			numOccurances += getNumOfOccurancesOfString line, searchTerm
 			if isEmailEnd(line)
 				break
 			end		
 		end 
-		f1.close
+		file.close
 	end
-	count 
+	numOccurances 
 end
 
-dir = Dir.glob('GYB-GMail-Backup-pietgeursen@gmail.com/*.eml')
-dir.sort!
 
-dir.each {
-	|eml_file|
-	count = processFile(eml_file)
-	#puts eml_file
-	puts count	
-}
-  # do work on files ending in .rb in the desired directory
+def processAllFilesInDirectory
+	score = {"Piet" => 0, "Babs" => 0}
+	@sender = ''
+
+
+	emails = Dir.glob('GYB-GMail-Backup-pietgeursen@gmail.com/*.eml')
+	emails.sort!
+
+	emails.each do |email|
+		count = searchEmailForNumOccurances(email, '!')
+
+		score[@sender] += count
+	#puts email
+	puts score["Piet"].to_s + ", " + score["Babs"].to_s	
+	end
+end
+
+#processAllFilesInDirectory
+
 
 
